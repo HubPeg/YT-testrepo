2964c8451a9f2d1a4a8600b74da679dd7123a795
Gianluca Petroni <codepeg.biz@gmail.com>
2025-12-26 09:38:09 +0100

    Version 2.0 - New Function

--- hello_world.py
+++ hello_world.py
@@ -1,2 +1,20 @@
 
 print("hello world")
+
+def write_name(name, surname):
+    sentence = f"Hello my name is {name} {surname}"
+
+    return sentence
+
+def padel_position(position):
+    position = position
+    game = f"{x} and I play on the {position} side"
+
+    return game
+
+
+x = write_name("Arturo", "Coello")
+y = padel_position("right")
+
+print(x)
+print(y)
